6b98d4be56f0681472d9f0464a244439bfb16a31
Albert <Albert.c.x.chen@oocl.com>
2020-07-26 23:51:21 +0800

    [parkingLot][story4][Albert] featrue :complete should_return_ticket_when_smartParkingBoy_parkCar_given_car_toMostIdleLot test

--- src/main/java/com/oocl/cultivation/smartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/smartParkingBoy.java
@@ -4,13 +4,14 @@ public class smartParkingBoy extends parkingBoy{
 
     @Override
     public carTicket parkCar(Car car) {
-        carTicket ticket =null;
+        carTicket ticket ;
         parkingLot mostIdelParkingLot = null;
 
         for (int i = 0; i <this.getParkingLotList().size()-1; i++) {
             if(this.getParkingLotList().get(i).getMap().size()>=this.getParkingLotList().get(i+1).getMap().size())
                 mostIdelParkingLot = this.getParkingLotList().get(i+1);
         }
+
 
         assert mostIdelParkingLot != null;
         ticket=mostIdelParkingLot.park(car);
